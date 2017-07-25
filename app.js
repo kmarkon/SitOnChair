16a5bfb4599ed9aaee94826af4f536332114a0b5
Katarzyna Waszczeniuk <kmarkon@gmail.com>
2017-07-25 14:34:12 +0200

    end

---
+++ app.js
@@ -0,0 +1,26 @@
+// $(function() {
+//
+//   var $box3_1 = $(".box3_1").offset().top;
+//   var $border_price = $(".border_price");
+//
+//   var $section4 = $(".section4").offset().top;
+//   var $border_contact = $(".border_contact");
+//
+//   $(window).scroll(function(event) {
+//
+//     var $scroll = $(window).scrollTop();
+//
+//     if ($scroll >= $box3_1) {
+//       $border_price.animate({
+//         width: "100%"
+//       }, "slow");
+//     }
+//     if ($scroll >= $section4) {
+//       $border_contact.animate({
+//         width: "100%"
+//       }, "slow");
+//     }
+//
+//   });
+//
+// })
